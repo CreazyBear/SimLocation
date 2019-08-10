42622eb02fe188548da4bd3699e88f5e6e5ff53d
熊伟 <597370561@qq.com>
2019-08-10 23:15:03 +0800

    Initial Commit

---
+++ SimLocation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SimLocation
+//
+//  Created by 熊伟 on 2019/8/10.
+//  Copyright © 2019 熊伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ SimLocation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimLocation
+//
+//  Created by 熊伟 on 2019/8/10.
+//  Copyright © 2019 熊伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
